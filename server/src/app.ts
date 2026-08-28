159e0ff79c0fb3f0ac3704db27530d49a2a1ea85
namgell-nepa <namgel.lama@nepa.global>
2026-08-28 14:50:24 +0545

    Configure cors and helmet

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,11 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import helmet from "@fastify/helmet";
 
 const app = fastify({ logger: true });
+
+app.register(cors);
+app.register(helmet);
 
 app.get("/health", async (request, reply) => {
     return { status: "ok" };
