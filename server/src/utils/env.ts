41065d08811cf5683d63958035bd88877ce8ee62
namgell-nepa <namgel.lama@nepa.global>
2026-08-28 19:58:52 +0545

    Configure redis and url queue

--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 class Env {
     static PORT = process.env.PORT ? Number(process.env.PORT) : 8000;
     static DATABASE_URL = process.env.DATABASE_URL || "";
+    static REDIS_URL = process.env.REDIS_URL || "";
 }
 
 export default Env;
