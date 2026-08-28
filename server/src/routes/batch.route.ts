e45da9d4e89066df388d2869be01f903750cbed4
namgell-nepa <namgel.lama@nepa.global>
2026-08-28 21:24:17 +0545

    Add batch urls to queue

--- server/src/routes/batch.route.ts
+++ server/src/routes/batch.route.ts
@@ -3,13 +3,15 @@ import { batchController } from "../controllers/batch.controller";
 import { batchRepository } from "../repositories/batch.repository";
 import { batchService } from "../services/batch.service";
 import { createBatchSchema } from "../validations/batch.validation";
+import { urlRepository } from "../repositories/url.repository";
 
 const batchRoutes = async (
     app: FastifyInstance,
     _opts: FastifyPluginOptions,
 ) => {
     const repository = batchRepository(app.prisma);
-    const service = batchService(repository);
+    const repositoryUrl = urlRepository(app.prisma);
+    const service = batchService(repository, repositoryUrl);
     const controller = batchController(service);
 
     app.get("/", controller.getAllBatches);
--- server/src/services/batch.service.ts
+++ server/src/services/batch.service.ts
@@ -1,8 +1,13 @@
+import { urlQueue } from "../queues/url.queue";
 import { type BatchRepository } from "../repositories/batch.repository";
+import type { UrlRepository } from "../repositories/url.repository";
 import { NotFoundError } from "../utils/error";
 import type { CreateBatchInput } from "../validations/batch.validation";
 
-export function batchService(batchRepository: BatchRepository) {
+export function batchService(
+    batchRepository: BatchRepository,
+    urlRepository: UrlRepository,
+) {
     return {
         getAll: () => {
             return batchRepository.getAll();
@@ -16,8 +21,35 @@ export function batchService(batchRepository: BatchRepository) {
             return batch;
         },
 
-        create: (body: CreateBatchInput) => {
-            return batchRepository.create(body);
+        create: async (body: CreateBatchInput) => {
+            const batch = await batchRepository.create(body);
+
+            const jobsWithIds = batch.urls.map((url) => ({
+                url,
+                jobId: `${url.id}-v${url.jobVersion}`,
+            }));
+
+            await urlQueue.addBulk(
+                jobsWithIds.map(({ url, jobId }) => ({
+                    name: "check-url",
+                    data: {
+                        urlId: url.id,
+                        batchId: batch.id,
+                        jobVersion: url.jobVersion,
+                    },
+                    opts: {
+                        jobId,
+                        attempts: 3,
+                        backoff: { type: "exponential", delay: 1000 },
+                    },
+                })),
+            );
+
+            await urlRepository.updateBulkJobIds(
+                jobsWithIds.map(({ url, jobId }) => ({ id: url.id, jobId })),
+            );
+
+            return batch;
         },
     };
 }
